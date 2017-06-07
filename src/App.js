18a7f5026af6aa0bde1e90f533fcba2e1f32916e
Minty <Ronak>
2017-06-07 12:21:09 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<h3 id='hello'>Hello</h3>
+			</div>
+		);
+	}
+}
+
+export default App;
