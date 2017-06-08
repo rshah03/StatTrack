2646ffd96b16efe017483979983d9789dd35fd84
Minty <Ronak>
2017-06-07 19:35:49 -0500

    Added event handler for form submit and fixed local table DOM to match V-DOM

--- src/Components/AddPlayer.js
+++ src/Components/AddPlayer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import uuid from 'uuid'
-import { Button } from 'react-bootstrap'
 
 class AddPlayer extends Component {
 	constructor() {
@@ -17,35 +16,41 @@ class AddPlayer extends Component {
 		LUK: 0
 	}
 	
-
+	handleSubmit(e) {
+		console.log('Event handler invoked')
+		e.preventDefault();
+		
+	}
 
 	render() {
 		return (
 			<div className='addPlayer'>
 				<h3>Add Player</h3>
-				<form onSubmit={console.log('Test')}>
+				<form onSubmit={this.handleSubmit.bind(this)}>
 					<div>
 						<table>
-							<tr>
-								<td>Name</td>
-								<td>: <input type='text' ref='name' /></td>
-							</tr>
-							<tr>
-								<td>INT</td>
-								<td>: <input type='text' ref='int' /></td>
-							</tr>
-							<tr>
-								<td>STR</td>
-								<td>: <input type='text' ref='str' /></td>
-							</tr>
-							<tr>
-								<td>DEX</td>
-								<td>: <input type='text' ref='dex' /></td>
-							</tr>
-							<tr>
-								<td>LUK</td>
-								<td>: <input type='text' ref='luk' /></td>
-							</tr>
+							<tbody>
+								<tr>
+									<td>Name</td>
+									<td>: <input type='text' ref='name' /></td>
+								</tr>
+								<tr>
+									<td>INT</td>
+									<td>: <input type='text' ref='int' /></td>
+								</tr>
+								<tr>
+									<td>STR</td>
+									<td>: <input type='text' ref='str' /></td>
+								</tr>
+								<tr>
+									<td>DEX</td>
+									<td>: <input type='text' ref='dex' /></td>
+								</tr>
+								<tr>
+									<td>LUK</td>
+									<td>: <input type='text' ref='luk' /></td>
+								</tr>
+							</tbody>
 						</table>
 					</div>
 					<input type='submit' value='submit' />
