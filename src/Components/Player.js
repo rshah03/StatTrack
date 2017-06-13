835a5f091312837bd9449568e5b72ad7ed7d77f3
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2017-06-13 01:37:34 -0500

    Second attempt at incrementing

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types'
 import PlayerItem from './PlayerItem'
 
 class Player extends Component {
+
+	incrementINT = () => {
+        let { player } = this.state;
+        player.int += 1;
+        this.setState({ player })
+    }
+
 	render() {
 		let playerItem
 		if(this.props.players) {
 			playerItem = this.props.players.map(player => {
 				return (
-					<PlayerItem key={player.name} player={player} />
+					<PlayerItem key={player.name} player={player} incrementINT={this.incrementINT} />
 				)
 			})
 		}
